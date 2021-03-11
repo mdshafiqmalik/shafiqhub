c34a733a0d96020479fda13cb297444a4134f931
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-03-11 21:16:43 +0530

    Sweeps some unwanted CSS  and JS

--- assets/js/error/404.js
+++ assets/js/error/404.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded",function(){
-  setTimeout( function(){
+document.addEventListener("DOMContentLoaded", function(){
+  setTimeout(function(){
     window.location = 'index.html';
-<<<<<<< HEAD
-  }, 10000);
-=======
-  }, 600000);
->>>>>>> 76a1e8298712f3fa3f41964c37ec5f8d111e0e4a
+  },50000)
 });
