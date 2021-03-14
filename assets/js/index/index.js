d5d6c284c7a8aa60b67db79b1a57f7ae7c5703f3
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-03-14 18:59:56 +0530

    Form added

--- assets/js/index/index.js
+++ assets/js/index/index.js
@@ -21,3 +21,58 @@ function toogleAnswer(FAQID){
 function goTo(link){
   window.location = `./${link}`;
 }
+let message = document.getElementById('statusMessage');
+$("#subsSubmit").click(function(){
+   $(".subscribeForm").submit(function(e) {
+    e.preventDefault();
+    });
+    let subsName = $("#subsName").val();
+    let subsEmail = $("#subsEmail").val();
+      let isValid = validateData(subsName, subsEmail);
+      console.log(isValid);
+      if (!isValid) {
+
+      }else {
+        message.innerHTML = "Sucessfully Subscribed";
+        message.classList.add("statusSucess");
+        message.classList.remove("statusWarning");
+      }
+  });
+function validateData(name, email){
+  let vname = validateName(name);
+  let vmail = validateEmail(email);
+  console.log("Valid Name: "+vname);
+  console.log("Valid email: "+vmail);
+  if (name.length == 0 || email.length == 0) {
+    message.innerHTML = "All fields required";
+    message.classList.add("statusWarning");
+    message.classList.remove("statusSucess");
+    return false;
+  }else if (vname && vmail) {
+    return true;
+  }else {
+    return false;
+  }
+}
+function validateName(name){
+  if (name.length < 4) {
+    message.innerHTML = "Name is too short";
+    message.classList.add("statusWarning");
+    message.classList.remove("statusSucess");
+    return false;
+  }else {
+    return true;
+  }
+}
+  function validateEmail(email){
+      let atpos = email.indexOf("@");
+      let dotpos = email.lastIndexOf(".");
+      if (atpos < 1 || (dotpos-atpos < 2) || email.length < 10) {
+        message.innerHTML = "Incorrect Email";
+        message.classList.add("statusWarning");
+        message.classList.remove("statusSucess");
+        return false;
+      }else {
+        return true;
+      }
+  }
