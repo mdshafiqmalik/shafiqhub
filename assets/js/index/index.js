5acfdd2ccb4f383e88d00213e133c850ba7eb08f
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-03-14 22:31:58 +0530

    HHE

--- assets/js/index/index.js
+++ assets/js/index/index.js
@@ -72,3 +72,17 @@ function validateName(name){
         return true;
       }
   }
+  var config = {
+      apiKey: "AIzaSyCKjB8oCGQ6oBWTdUQea18vsJLIAsU5NFE",
+      authDomain: "shafiqhub-db.firebaseapp.com",
+      databaseURL: "https://shafiqhub-db-default-rtdb.firebaseio.com",
+      projectId: "shafiqhub-db",
+      storageBucket: "shafiqhub-db.appspot.com",
+      messagingSenderId: "647856676003",
+      appId: "1:647856676003:web:04d1b539d00471b2db7316",
+      measurementId: "G-Z5TN9P43EY"
+    };
+  firebase.initializeApp(config);
+  firebase.database().ref("/get/all/the/data").once("value", function (data) {
+      console.log(data.val());
+  });
