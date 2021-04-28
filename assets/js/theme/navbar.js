9f09f2ad5e3e99e012ba8d48959b0d80d50ea084
Mohd Shafiq Malik <63715841+mdshafiqmalik@users.noreply.github.com>
2021-04-28 06:09:26 +0530

    Update navbar.js

--- assets/js/theme/navbar.js
+++ assets/js/theme/navbar.js
@@ -2,6 +2,7 @@
 'use strict';
 // -- :: Loading Page
 $(window).on('load', function () {
+  $('#l-m').html("loading...");
   $('#loading').fadeOut(1000);
   $('html').css('overflow-y', 'visible');
 });
