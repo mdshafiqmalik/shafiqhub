aa2f7ba753e21f2f13b4dfbd1b5310c90c9e3495
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-05-05 21:54:32 +0530

    dbj

--- assets/js/index/index.js
+++ assets/js/index/index.js
@@ -72,21 +72,21 @@ function validateName(name){
         return true;
       }
   }
-  var config = {
-      apiKey: "AIzaSyCKjB8oCGQ6oBWTdUQea18vsJLIAsU5NFE",
-      authDomain: "shafiqhub-db.firebaseapp.com",
-      databaseURL: "https://shafiqhub-db-default-rtdb.firebaseio.com",
-      projectId: "shafiqhub-db",
-      storageBucket: "shafiqhub-db.appspot.com",
-      messagingSenderId: "647856676003",
-      appId: "1:647856676003:web:04d1b539d00471b2db7316",
-      measurementId: "G-Z5TN9P43EY"
-    };
-  firebase.initializeApp(config);
-  firebase.database().ref("/get/all/the/data").once("value", function (data) {
-      console.log(data.val());
-  });
-  var mykey = configr.MY_KEY;
-  var secretkey = configr.SECRET_KEY;
+  // var config = {
+  //     apiKey: "AIzaSyCKjB8oCGQ6oBWTdUQea18vsJLIAsU5NFE",
+  //     authDomain: "shafiqhub-db.firebaseapp.com",
+  //     databaseURL: "https://shafiqhub-db-default-rtdb.firebaseio.com",
+  //     projectId: "shafiqhub-db",
+  //     storageBucket: "shafiqhub-db.appspot.com",
+  //     messagingSenderId: "647856676003",
+  //     appId: "1:647856676003:web:04d1b539d00471b2db7316",
+  //     measurementId: "G-Z5TN9P43EY"
+  //   };
+  // firebase.initializeApp(config);
+  // firebase.database().ref("/get/all/the/data").once("value", function (data) {
+  //     console.log(data.val());
+  // });
+  var mykey = config.MY_KEY;
+  var secretkey = config.SECRET_KEY;
   console.log(mykey);
   console.log(secretkey);
