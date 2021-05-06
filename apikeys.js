af0dca2a127791d9ef9dc2102115ac3329ea6bd1
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-05-06 12:54:53 +0530

    jusdhfi

--- apikeys.js
+++ apikeys.js
@@ -1,4 +1,4 @@
-module.exports =  { apiConfig };
+
 let apiConfig = {
     "apiKey": "AIzaSyCKjB8oCGQ6oBWTdUQea18vsJLIAsU5NFE",
     "authDomain": "shafiqhub-db.firebaseapp.com",
@@ -9,3 +9,4 @@ let apiConfig = {
     "appId": "1:647856676003:web:04d1b539d00471b2db7316",
     "measurementId": "G-Z5TN9P43EY"
   };
+module.exports =  apiConfig;
