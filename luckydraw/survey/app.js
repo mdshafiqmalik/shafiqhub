203786d9a3010021e7a6b888e57ddac384e90327
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-05-14 17:52:40 +0530

    jh

---
+++ luckydraw/survey/app.js
@@ -0,0 +1,8 @@
+
+// console.log(q1[0].value);
+function submitAnswer() {
+  let q1 = document.getElementsByTagName('input');
+  for (var i = 0; i < q1.length; i++) {
+    console.log(q1[i].value);
+  }
+}
