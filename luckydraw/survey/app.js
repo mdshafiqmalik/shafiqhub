f20c56de22f85f841bc289121c8846377a8830ae
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-05-15 20:02:34 +0530

    fd

--- luckydraw/survey/app.js
+++ luckydraw/survey/app.js
@@ -1,10 +1,8 @@
 function submitAnswer(){
 document.getElementById('questions').innerHTML = "<p class='topic'>To get your money you need to share it with 10 whatsapp contacts/groups.<br><br>When you share you will be rewarded by<b style='color:lime;'> &#8377; 367.42 ($5) </b>into your paytm or paypal wallet.<br><br><span id='times'></span></p><center><br><progress id='pbar' value='1' max='12'></progress><br><button id='ssubmit' href='whatsapp://send?text=http://earnmore.tech/survey'  onclick='wa()' type='button'> Share </button></center> <!-- pf 02 --><div class='ads'><ins class='adsbygoogle'style='display:block'data-ad-format='fluid'data-ad-layout-key='-fb+5w+4e-db+86'data-ad-client='ca-pub-2950653490323756'data-ad-slot='1422493112'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>";
 }
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-KXERHN07TF');
-</script>
