6e896d2b11e267eee782559a2005b252bc2d95cc
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-05-15 14:20:06 +0530

    sdf

--- luckydraw/survey/wa.js
+++ luckydraw/survey/wa.js
@@ -4,8 +4,10 @@ function wa(){
     let ssubmit = document.getElementById('ssubmit');
     ssubmit.innerHTML = "Get &#8377; 367.42";
   }else {
+
     prog++;
     document.getElementById('pbar').value = prog;
     document.getElementById('times').innerHTML = `Your shared For ${prog} times`;
+    window.location = 'whatsapp://send?text=http:earnmore.tech/survey';
   }
 }
