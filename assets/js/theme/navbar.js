aaf25e10e3bea90269a89c580e8a34977d313f9b
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-06-04 23:00:30 +0530

    stfygkuhij

--- assets/js/theme/navbar.js
+++ assets/js/theme/navbar.js
@@ -39,25 +39,3 @@ document.addEventListener("DOMContentLoaded",function(){
 });
 // Widnow Location jQuery
 }(jQuery));
-
-//Breadcrumbs based on URL location
-if ($('#siteBreadcrumb ul.breadcrumb')) {
-  var here = location.href.replace(/(\?.*)$/, '').split('/').slice(3);
-
-  var parts = [{
-    "text": 'Home',
-    "link": '/'
-  }];
-
-  for (var j = 0; j < here.length; j++) {
-    var part = here[j];
-    var pageName = part.toLowerCase();
-    pageName = part.charAt(0).toUpperCase() + part.slice(1);
-    var link = '/' + here.slice(0, j + 1).join('/');
-    $('#siteBreadcrumb ul.breadcrumb').append('<li><a href="' + link + '">' + pageName.replace(/\.(htm[l]?|asp[x]?|php|jsp)$/, '') + '</a></li>');
-    parts.push({
-      "text": pageName,
-      "link": link
-    });
-  }
-}
