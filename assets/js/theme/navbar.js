2a951a8ab2ca191fdeec7b318dd3a0f2e08ffd4f
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2021-06-18 18:14:03 +0100

    saerdgfhg

--- assets/js/theme/navbar.js
+++ assets/js/theme/navbar.js
@@ -1,4 +1,6 @@
 (function ($) {
+  let mobToc = document.getElementById('mobToc');
+  let deskToc = document.getElementById('sb');
 'use strict';
 // -- :: Loading Page
 $('html').css('overflow-y', 'visible');
@@ -7,16 +9,41 @@ $('html').css('overflow-y', 'visible');
 $(window).on('scroll', function () {
   if($(window).scrollTop() > 20 ) {
     $('.top-bar').removeClass('d-lg-block');
+    $('.sp').css('top','.5rem');
     $('nav').addClass('scrolled');
-  } else {
+  }else {
     $('nav').removeClass('scrolled');
     $('.top-bar').addClass('d-lg-block');
+    $('.sp').css('top','auto');
   }
 });
+$(window).on('scroll', function (){
+  if($(window).scrollTop() > 40 ) {
+    mobToc.innerHTML = "";
+    deskToc.innerHTML = breadCrumb;
+  }else {
+    mobToc.innerHTML = breadCrumb;
+    deskToc.innerHTML = "";
+  }
+});
+
+
 
 $('nav button').on('click', function() {
   $($(this).data('target')).fadeToggle();
 });
+// Posts
+document.addEventListener("resize", function(){
+  if($(document).width() < 991) {
+      mobToc.innerHTML = breadCrumb;
+      deskToc.innerHTML = "";
+  }
+  else{
+    mobToc.innerHTML = "";
+    deskToc.innerHTML = breadCrumb;
+  }
+});
+
 document.addEventListener("DOMContentLoaded",function(){
   if ($(document).width() < 974) {
     // Add slideDown animation to Bootstrap dropdown when expanding.
